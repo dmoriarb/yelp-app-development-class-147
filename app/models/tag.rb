404b6b26a2883d1241e22cbbbad37a1bcaa6204a
Raghu Betina <raghu@firstdraft.com>
2016-09-27 18:26:55 +0000

    Direct association between tags and kind_of_foods

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,10 @@
 class Tag < ApplicationRecord
   # Direct associations
 
+  has_many   :kind_of_foods,
+             :foreign_key => "tags_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
